4e4fcb1ec6197eab4dc3a6a47a83b6238dfa6393
Arthur Wu <arthur.wu@quest.com>
2012-11-22 13:14:43 +0800

    add apicall app

---
+++ apicall/model.py
@@ -0,0 +1,22 @@
+import web
+from datetime import datetime
+import os
+DB_PATH = os.path.join(os.path.dirname(__file__), 'data.sqlite')
+
+db = web.database(dbn='sqlite', db=DB_PATH)
+
+def get_urls():
+	return db.select('url', order='created')
+	
+def get_url(url):
+	try:
+		return db.select('url', where='url=$url', vars=locals())[0]
+	except IndexError:
+		return None;
+
+def add_url_if_not_exist(url, user, password):
+	exist = db.select('url', locals(), where='url=$url and user=$user and password=$password')
+	if not exist: db.insert('url', url=url, user=user, password=password, created=datetime.now())
+	
+def del_url(url):
+	return db.delete('url', where='url=$url', vars=locals())
